e36c33b7b19dec8a360b2fd02aec50ab00269388
Marco Mangan <marco.mangan@pucrs.br>
2019-09-04 13:38:21 -0300

    #1

---
+++ src/main/java/com/bdd/linkedlist/Node.java
@@ -0,0 +1,24 @@
+package com.bdd.linkedlist;
+public class Node {
+  private String data;
+  private Node next;
+  public String getData() {
+    return data;
+  }
+  public void setData(String data) {
+    this.data = data;
+  }
+  public Node getNext() {
+    return next;
+  }
+  public void setNext(Node next) {
+    this.next = next;
+  }
+  public Node(String data, Node next){
+    this.data= data;
+    this.next= next;
+  }
+  public String toString(){
+    return data;
+  }
+}
